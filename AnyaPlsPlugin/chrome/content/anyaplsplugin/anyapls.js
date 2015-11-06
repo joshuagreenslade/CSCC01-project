bb32efaa73291c6e028e333d440b32d8a44b3f6e
Kaida Lin <kaida7189@gmail.com>
2015-11-05 22:36:18 -0500

    Add file for AnyaPlsPlugin

---
+++ AnyaPlsPlugin/chrome/content/anyaplsplugin/anyapls.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Kaida on 05/11/2015.
+ *
+ * This is the main class of our "AnyaPls" plugin which have feature customField, BatchEditing, etc
+ * Every time a new feature is added, please create a new "featurename.js" and "featurename.xul"
+ */
+
+Zotero.AnyaPls = {
+    DB: null,
+    init: function () {
+        this.DB = new Zotero.DBConnection('anyaPls');
+        if (!this.DB.tableExists("customField")) {
+            this.DB.query("CREATE TABLE " + "customField" + " (itemID INTEGER, fieldName TEXT, fieldValue TEXT)");
+        }
+    },
+
+    getZoteroPane: function() {
+        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+            .getService(Components.interfaces.nsIWindowMediator);
+        var enumerator = wm.getEnumerator("navigator:browser");
+        while (enumerator.hasMoreElements()) {
+            var win = enumerator.getNext();
+            if (!win.ZoteroPane) continue;
+            return win.ZoteroPane;
+        }
+    },
+
+    customField: function(){
+        var ZoteroPane = Zotero.AnyaPls.getZoteroPane();
+        var items = ZoteroPane.getSelectedItems();
+
+        if (items[0] != null) {
+            //window.alert("item has been selected");
+            window.openDialog("chrome://anyaplsplugin/content/customform.xul", "", "chrome, dialog=0, modal, centerscreen");
+        } else {
+            window.alert("item has not been selected");
+            return false;
+        }
+    },
+
+    batchEditing: function() {
+
+    }
+
+};
+
---
+++ AnyaPlsPlugin/chrome/content/anyaplsplugin/include.js
@@ -0,0 +1,10 @@
+var Zotero = Components.classes["@zotero.org/Zotero;1"]
+	.getService(Components.interfaces.nsISupports)
+	.wrappedJSObject;
+
+if (!Zotero.AnyaPls) {
+	const loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+			.getService(Components.interfaces.mozIJSSubScriptLoader);
+	loader.loadSubScript("chrome://anyaplsplugin/content/anyapls.js");
+	Zotero.AnyaPls.init();
+}
