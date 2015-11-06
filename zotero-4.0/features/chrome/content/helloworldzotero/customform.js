d1f7edb0740d8a244b7208ca33f45ccbfbac35e2
joshuagreenslade <joshua.greenslade@mail.utoronto.ca>
2015-11-05 22:24:20 -0500

    fixed custom field delete backend to properly delete the field from the database

--- zotero-4.0/features/chrome/content/helloworldzotero/customform.js
+++ zotero-4.0/features/chrome/content/helloworldzotero/customform.js
@@ -38,8 +38,18 @@ Zotero_AnyaPls_CustomForm.modify = function() {
 };
 
 Zotero_AnyaPls_CustomForm.delete = function() {
-    //use this sql
-    var sql_delete = "DELETE FROM customField WHERE itemID=? AND fieldName=?";
+
+    var ZoteroPane = Components.classes["@mozilla.org/appshell/window-mediator;1"] .getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser").ZoteroPane;
+
+    var field = document.getElementById('field').value;
+    var value = document.getElementById('value').value;
+    var item = ZoteroPane.getSelectedItems()[0];
+
+    this.DB = new Zotero.DBConnection('anyaPls');
+    var sql_delete = "DELETE FROM " + "customField" + " WHERE itemID=" + item.id + " AND fieldName='" + field + "' AND fieldValue ='" + value + "'";
+
+    console.log("deleting Field: " + field);
+    this.DB.query(sql_delete);
 
     //Can remove window.close() if only want to close window when click OK.
     //use  Zotero.AnyaPls.DB.query(sql_delete, itemID, field);
