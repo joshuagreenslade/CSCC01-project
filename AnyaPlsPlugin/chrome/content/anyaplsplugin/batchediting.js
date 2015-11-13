a199e0f3c6761b3867059032e481ed485b8691a1
joshuagreenslade <joshua.greenslade@mail.utoronto.ca>
2015-11-13 02:01:36 -0500

    implemented displaying all tags in the edit tags window

--- AnyaPlsPlugin/chrome/content/anyaplsplugin/batchediting.js
+++ AnyaPlsPlugin/chrome/content/anyaplsplugin/batchediting.js
@@ -9,96 +9,78 @@ logger = function(msg) {
         .getService(Components.interfaces.nsIConsoleService);
     consoleService.logStringMessage(msg);
 };
+
 Zotero_AnyaPls_BatchEdit.init = function () {
     updateDisplay();
 };
 
 updateDisplay = function() {
 
-    var displayBox = document.getElementById('field-display-box');
-    var sql_search = "SELECT DISTINCT fieldName,fieldValue FROM customField";
+    var displayBox = document.getElementById('tag-display-box');
 
-    //remove all custom fields from the listbox
+    //remove all tags from the listbox
     var total_rows = displayBox.getRowCount();
     for(var i = 0; i < total_rows; i++) {
         displayBox.removeItemAt(0);
     }
 
-    //put each custom field into the listbox
-    //this updates the listbox to show the current list of custom fields
-    var result = Zotero.AnyaPls.DB.query(sql_search);
-    for(var i = 0; i < result.length; i++) {
-        displayBox.appendItem(result[i].fieldName + ": " + result[i].fieldValue);
-    }
-};
-
-Zotero_AnyaPls_BatchEdit.selectField = function() {
-    var displayBox = document.getElementById('field-display-box');
-
-    //the item selected from the display box
-    var selected_field = displayBox.selectedItem;
-
-    //put the selected field and value into the field and value textboxes
-    document.getElementById("field").value = selected_field.label.split(": ")[0];
-    document.getElementById("value").value = selected_field.label.split(": ")[1];
-};
-
-Zotero_AnyaPls_BatchEdit.add = function() {
-
-    var field = document.getElementById("field");
-    var value = document.getElementById("value");
-    var sql_add = "INSERT INTO " + "customField" + " VALUES (?,?,?)";
-    var ZoteroPane = Zotero.AnyaPls.getZoteroPane();
-    var items = ZoteroPane.getSelectedItems();
-
-    //Insert new field to all selected item
-    for (var i = 0; i < items.length; i++) {
-        Zotero.AnyaPls.DB.query(sql_add, [items[i].id, field.value, value.value]);
+    //put each tag into the listbox
+    //this updates the listbox to show the current list of tags
+    var all_tags = Zotero.Tags.search();
+    for (var id in all_tags) {
+        displayBox.appendItem(all_tags[id].name, id);
     }
 
-    //reset the text boxes
-    field.value = '';
-    value.value = '';
-
-    //update the list of fields
-    updateDisplay();
 };
 
-Zotero_AnyaPls_BatchEdit.modify = function() {
+Zotero_AnyaPls_BatchEdit.merge = function() {
 
-    var ZoteroPane = Zotero.AnyaPls.getZoteroPane();
-    var items = ZoteroPane.getSelectedItems();
-    var field = document.getElementById('field');
-    var value = document.getElementById('value');
-    var sql_modify = "UPDATE customField SET fieldValue=? WHERE itemID=? AND fieldName=?";
-    for (var i = 0; i<items.length; i++) {
-        Zotero.AnyaPls.DB.query(sql_modify, [value.value, items[i].id, field.value]);
-    }
+};
 
-    //reset the text boxes
-    field.value = '';
-    value.value = '';
+Zotero_AnyaPls_BatchEdit.rename = function() {
 
-    //update the list of fields
-    updateDisplay();
 };
 
 Zotero_AnyaPls_BatchEdit.delete = function() {
 
     var ZoteroPane = Zotero.AnyaPls.getZoteroPane();
-    var items = ZoteroPane.getSelectedItems();
-    var field = document.getElementById('field');
-    var value = document.getElementById('value');
-    var sql_delete = "DELETE FROM customField WHERE itemID=? AND fieldName=? AND fieldValue=?";
+    var selected_tags = document.getElementById('tag-display-box').selectedItems;
 
-    for (var i = 0; i<items.length; i++) {
-        Zotero.AnyaPls.DB.query(sql_delete, [items[i].id, field.value, value.value]);
+    //a message for the confirmation window
+    var message = "Are you sure you want to delete the following tags:\n";
+
+    //add the labels of the selected tags to the message and get the ids of the selected tags
+    for(var i = 0; i < selected_tags.length; i++) {
+        message = message + selected_tags[i].label + ", ";
+        selected_tags[i] = selected_tags[i].value;
     }
 
-    //reset the text boxes
-    field.value = '';
-    value.value = '';
+    //remove the last comma and add the remaining message
+    message = message.substring(0, message.length - 2);
+    message = message + "?\nThey will be removed from all items.";
 
-    //update the list of fields
+    //a window that confirms that the user wants to delete the selected tags
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+        .getService(Components.interfaces.nsIPromptService);
+    var confirmed = promptService.confirm(window, "Delete Tags", message);
+
+    if (confirmed) {
+        Zotero.DB.beginTransaction();
+
+        //delete the tags and update the database
+        if (selected_tags.length) {
+            Zotero.Tags.erase(selected_tags);
+            Zotero.Tags.purge(selected_tags);
+        }
+
+        Zotero.DB.commitTransaction();
+
+        // If only a tag color setting, remove that
+        if (!selected_tags.length) {
+            Zotero.Tags.setColor(this.libraryID, name, false);
+        }
+    }
+
+    //update the list of tags
     updateDisplay();
 };
