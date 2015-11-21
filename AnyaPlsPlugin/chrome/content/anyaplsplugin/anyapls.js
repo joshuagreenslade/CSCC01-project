6e5308929db68950ec7fbe5fffb4cf8a1ecd5903
joshuagreenslade <joshua.greenslade@mail.utoronto.ca>
2015-11-21 11:01:15 -0500

    implemented custom field search that kindof works

--- AnyaPlsPlugin/chrome/content/anyaplsplugin/anyapls.js
+++ AnyaPlsPlugin/chrome/content/anyaplsplugin/anyapls.js
@@ -77,14 +77,16 @@ Zotero.AnyaPls = {
 
         //maybe only do this if tags and fields or everything is selected
 
-        var checkbox = document.getElementById('custom-field-search-checkbox')
-        var searchbar = document.getElementById('zotero-tb-search')
+        var checkbox = document.getElementById('custom-field-search-checkbox');
+        var searchbar = document.getElementById('zotero-tb-search');
 
         if(checkbox.hasAttribute('checked')) {
-            searchbar.setAttribute('oncommand', 'ZoteroPane_Local.search(), Zotero.AnyaPls.searchCustomFields()');
+            searchbar.setAttribute('oncommand', 'Zotero.AnyaPls.searchCustomFields()');
+            console.log("checked");
         }
         else {
             searchbar.setAttribute('oncommand', 'ZoteroPane_Local.search()');
+            console.log("unchecked");
         }
     },
 
@@ -97,37 +99,57 @@ Zotero.AnyaPls = {
         var results = this.DB.query(sql_search);
 
         //get the items that correspond to the selected itemID's selected from the customField table
-        var items = [];
+        var ids = [];
         for(var i=0; i < results.length; i++) {
-            items.push(Zotero.Items.get(results[i].itemID));
+            console.log(i);
+            ids.push(results[i].itemID);
         }
-        console.log(items);
+        console.log(ids);
 
         //display the items in the zotero display window
 
         //figure out how to display the items that were selected
-        /*
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//first figure out how to add the normal search results to the database searched results then figure out how to not crash it//
+//doesnt work on full screen at all                                                                                         //
+//                                                                                                                          //
+//if all else fails will have to open a window which displays the titles of the objects with the specified field            //
+//so maybe make a backup first                                                                                              //
+//          search custom fields button that opens a window where you enter text and in the window the title is displayed   //
+//          (not the tree thing because thats really hard) and possibly the field/value that was found in the database      //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //maybe use ZoteroPane.itemsView.setFilter('search', 's'), setFiter is in itemTreeView.js
+
+
+        //currently works but crashes with error
+        //"NS_ERROR_UNEXPECTED: Component returned failure code: 0x8000ffff (NS_ERROR_UNEXPECTED) [nsITreeSelection.select]"
+        //if you try to click on something
+        //after you search once for custom fields
+
+        //once you search a custom field you can nolonger click on something or do a normal search again
+        //once this function is run aleast once clicking items and normal search is no longer avaliable
+
+        var _libraryID;
+        this.itemsView = false;
+
          var itemGroup = {
-         //            ref: {
-         //                libraryID: _libraryID
-         //            },
-         isSearchMode: function() { return true; },
-         getItems: function (items) {
-         return items;
-         },
+             ref: {
+                 libraryID: _libraryID
+             },
+        isSearchMode: function() { return true; },
+         getItems: function (items) { return Zotero.Items.get(ids); },
          isLibrary: function () { return false; },
-         isCollection: function () { return false; },
-         isSearch: function () { return true; },
-         isShare: function () { return false; },
-         isTrash: function () { return false; }
-         }
-
-         if (this.itemsView) {
-         this.itemsView.unregister();
-         }
-
+        isCollection: function () { return false; },
+        isSearch: function () { return true; },
+        isShare: function () { return false; },
+        isTrash: function () { return false; }
+        }
+  //       if (this.itemsView) {
+  //           this.itemsView.unregister();
+  //       }
          this.itemsView = new Zotero.ItemTreeView(itemGroup, false);
          document.getElementById('zotero-items-tree').view = this.itemsView;
-         */
     }
 };
