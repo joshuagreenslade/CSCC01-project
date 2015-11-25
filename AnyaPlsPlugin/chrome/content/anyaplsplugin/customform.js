17f9309b33b4bc33b8d6a82df32b84cfb5558eaa
joshuagreenslade <joshua.greenslade@mail.utoronto.ca>
2015-11-24 22:13:12 -0500

    fixed the add custom fields window so you can add the splitters as fields

--- AnyaPlsPlugin/chrome/content/anyaplsplugin/customform.js
+++ AnyaPlsPlugin/chrome/content/anyaplsplugin/customform.js
@@ -27,26 +27,36 @@ updateDisplay = function() {
     }
 
     //column indicator/separator
-    displayBox.appendItem("\n<----Current item's fields---->");
+    displayBox.appendItem("<----Current item's fields---->");
+    displayBox.getItemAtIndex(0).disabled = true;
+
 
     var result = Zotero.AnyaPls.DB.query(sql_search_with_item);
 
     //if column has no fields, display (none)
-    if(!result.length)
+    if(!result.length) {
         displayBox.appendItem("(insert new fields)");
+        displayBox.getItemAtIndex(displayBox.getRowCount()-1).disabled = true;
+    }
     else
         for(var i = 0; i < result.length; i++) {
             displayBox.appendItem(result[i].fieldName + ": " + result[i].fieldValue);
         }
 
     //column indicator/separator
+    displayBox.appendItem(" ");
     displayBox.appendItem("<----Other custom fields---->");
+    displayBox.getItemAtIndex(displayBox.getRowCount()-1).disabled = true;
+    displayBox.getItemAtIndex(displayBox.getRowCount()-2).disabled = true;
+
     //put each custom field into the listbox
     //this updates the listbox to show the current list of custom fields
     var result = Zotero.AnyaPls.DB.query(sql_search);
 
-    if(!result.length)
+    if(!result.length) {
         displayBox.appendItem("(none)");
+        displayBox.getItemAtIndex(displayBox.getRowCount() - 1).disabled = true;
+    }
     else
         for(var i = 0; i < result.length; i++) {
             displayBox.appendItem(result[i].fieldName + ": " + result[i].fieldValue);
@@ -78,14 +88,17 @@ updateDisplay = function() {
 
 Zotero_AnyaPls_CustomForm.selectField = function() {
     var displayBox = document.getElementById('field-display-box');
-    var displayBox2 = document.getElementById('display-box');
+    //var displayBox2 = document.getElementById('display-box');
 
     //the item selected from the display box
-    var selected_field = displayBox.selectedItem || displayBox2.selectedItem;
+    var selected_field = displayBox.selectedItem;// || displayBox2.selectedItem;
 
-    //put the selected field and value into the field and value textboxes
-    document.getElementById("field").value = selected_field.label.split(": ")[0];
-    document.getElementById("value").value = selected_field.label.split(": ")[1];
+    if(selected_field.disabled == false) {
+
+        //put the selected field and value into the field and value textboxes
+        document.getElementById("field").value = selected_field.label.split(": ")[0];
+        document.getElementById("value").value = selected_field.label.split(": ")[1];
+    }
 };
 
 Zotero_AnyaPls_CustomForm.add = function() {
